ef021cfb879be07392b2e608d377c7d81f6ae27e
xiaowang <2805528068@qq.com>
2022-08-19 17:46:16 +0800

    二维数组

--- 专业课/C语言/04_数组/二维数组/01_二维数组的定义和使用.c
+++ 专业课/C语言/04_数组/二维数组/01_二维数组的定义和使用.c
@@ -23,19 +23,21 @@
 void InitArray(int y,int a[][y],int x){
 	srand((unsigned int)time(NULL));
 	for(int i=0;i<x;i++){
-		for(int j=0;j<y;i++){
+		for(int j=0;j<y;j++){
 			a[i][j] = rand()%10;
-			printf("%d ",a[i][j]);
 		}
 	}
 	printArray(x,y,a);
 }
 
 int main(){
+	static int a0[2][3]; //static 修饰全为0
 	int a[4][30];//4个班级，每个班30个同学----初始化未赋值，随机数---残留值
-//	InitArray(30,a,4);
+	InitArray(30,a,4);
 	
 	int b[2][3] = {1,2,4}; //部分赋值----补零
-	int b[2][3] = {1,2,4,5,6,7};//整体赋值
-	printArray(2,3,b); //1,2,4;0,0,0
+	int b1[2][3] = {1,0};//部分赋值----补零
+	int b2[2][3] = {1,2,4,5,6,7};//整体赋值
+	
+	printArray(2,3,a0); //1,2,4;0,0,0
 }
